e6d551e6bd2defcebb488e4699af02ac716aab80
Palash <palashgdev@gmail.com>
2022-07-26 17:26:41 +0530

    feat: icons are updated

---
+++ src/asset/icons/icons/whysignozcard.tsx
@@ -0,0 +1,203 @@
+import { IconProps } from "..";
+
+const WhySignozCard = (props: IconProps) => {
+  return (
+    <svg
+      width="82"
+      height="92"
+      viewBox="0 0 82 92"
+      fill="none"
+      className={props.className}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d_100_1308)">
+        <path
+          d="M17.6729 70.3295V12.6704H53.603V23.011H64.107V70.3295H17.6729Z"
+          fill="white"
+        />
+        <path d="M54.0127 22.7273V12L64.78 22.7273H54.0127Z" fill="#A3B4CB" />
+        <path
+          d="M54.0127 13.3408V20.3706C54.0127 21.8572 55.2179 23.0624 56.7045 23.0624H64.1071"
+          stroke="#A3B4CB"
+          strokeWidth="2.69183"
+        />
+        <g filter="url(#filter1_d_100_1308)">
+          <path
+            d="M19.6918 13.3459H53.3496C53.7055 13.3459 54.0469 13.4869 54.2991 13.7379L63.3742 22.7715C63.6279 23.0241 63.7706 23.3673 63.7706 23.7254V68.3082C63.7706 69.0515 63.168 69.6541 62.4247 69.6541H19.6918C18.9485 69.6541 18.3459 69.0515 18.3459 68.3082V14.6918C18.3459 13.9485 18.9485 13.3459 19.6918 13.3459Z"
+            stroke="#A3B4CB"
+            strokeWidth="2.69183"
+            shapeRendering="crispEdges"
+          />
+        </g>
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M30.3315 34.8394C30.0943 34.737 29.9853 34.4614 30.0884 34.2244L36.7989 18.7962C36.9016 18.5602 37.1759 18.4517 37.4123 18.5537L37.6974 18.6769C37.9347 18.7793 38.0436 19.0549 37.9406 19.2919L31.2301 34.72C31.1274 34.9561 30.8531 35.0646 30.6167 34.9625L30.3315 34.8394Z"
+          fill="#A3B4CB"
+        />
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M28.1465 22.616C28.3287 22.4344 28.6235 22.4344 28.8057 22.616L29.0246 22.8341C29.2078 23.0166 29.2078 23.3133 29.0246 23.4958L26.0488 26.4606C25.9266 26.5822 25.9266 26.78 26.0488 26.9017L29.0246 29.8665C29.2078 30.049 29.2078 30.3456 29.0246 30.5281L28.8057 30.7462C28.6235 30.9278 28.3287 30.9278 28.1465 30.7462L24.5089 27.1222C24.2647 26.8789 24.2647 26.4833 24.5089 26.24L28.1465 22.616Z"
+          fill="#A3B4CB"
+        />
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M41.7241 22.616C41.5419 22.4344 41.2471 22.4344 41.0649 22.616L40.846 22.8341C40.6628 23.0166 40.6628 23.3133 40.846 23.4958L43.8218 26.4606C43.944 26.5822 43.944 26.78 43.8218 26.9017L40.846 29.8665C40.6628 30.049 40.6628 30.3456 40.846 30.5281L41.0649 30.7462C41.2471 30.9278 41.5419 30.9278 41.7241 30.7462L45.3617 27.1222C45.6059 26.8789 45.6059 26.4833 45.3617 26.24L41.7241 22.616Z"
+          fill="#A3B4CB"
+        />
+        <rect
+          x="24.9468"
+          y="39.3979"
+          width="5.91551"
+          height="1.70601"
+          rx="0.853007"
+          fill="#A3B4CB"
+        />
+        <rect
+          x="33.9756"
+          y="39.3979"
+          width="11.364"
+          height="1.70601"
+          rx="0.853007"
+          fill="#A3B4CB"
+        />
+        <rect
+          x="36.3108"
+          y="44.8267"
+          width="14.9444"
+          height="1.70601"
+          rx="0.853007"
+          fill="#A3B4CB"
+        />
+        <rect
+          x="38.6458"
+          y="50.2549"
+          width="7.93923"
+          height="1.70601"
+          rx="0.853007"
+          fill="#A3B4CB"
+        />
+        <rect
+          x="47.9861"
+          y="50.2549"
+          width="9.65162"
+          height="1.70601"
+          rx="0.853007"
+          fill="#A3B4CB"
+        />
+        <rect
+          x="24.9468"
+          y="55.6831"
+          width="5.91551"
+          height="1.70601"
+          rx="0.853007"
+          fill="#A3B4CB"
+        />
+        <rect
+          x="33.9756"
+          y="55.6831"
+          width="11.364"
+          height="1.70601"
+          rx="0.853007"
+          fill="#A3B4CB"
+        />
+        <rect
+          x="36.3108"
+          y="61.1113"
+          width="14.9444"
+          height="1.70601"
+          rx="0.853007"
+          fill="#A3B4CB"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_100_1308"
+          x="0.961165"
+          y="0.543689"
+          width="80.1941"
+          height="91.0777"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feMorphology
+            radius="2.29126"
+            operator="dilate"
+            in="SourceAlpha"
+            result="effect1_dropShadow_100_1308"
+          />
+          <feOffset dy="4.58252" />
+          <feGaussianBlur stdDeviation="6.87379" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_100_1308"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_100_1308"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter1_d_100_1308"
+          x="0.961165"
+          y="0.543689"
+          width="80.1941"
+          height="91.0777"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feMorphology
+            radius="2.29126"
+            operator="dilate"
+            in="SourceAlpha"
+            result="effect1_dropShadow_100_1308"
+          />
+          <feOffset dy="4.58252" />
+          <feGaussianBlur stdDeviation="6.87379" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_100_1308"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_100_1308"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
+
+export default WhySignozCard;
