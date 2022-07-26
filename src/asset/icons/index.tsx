c8a51d27af1f604f4c4a9662b2c9ffdf67cb80db
Palash <palashgdev@gmail.com>
2022-07-26 17:25:48 +0530

    feat: icons are added

--- src/asset/icons/index.tsx
+++ src/asset/icons/index.tsx
@@ -1,7 +1,25 @@
 import Minus from "./icons/minus";
 import Plus from "./icons/plus";
+import WhySignozCard from "./icons/whysignozcard";
+import ShieldIcon from "./icons/shield";
+import DataStorage from "./icons/dataStorage";
+import NoComplianceDocument from "./icons/noComplianceDocument";
+import ShieldWithTick from "./icons/shieldWithTick";
+import ComplianceDocumentTick from "./icons/complianceDocumentTick";
+import Share from "./icons/Share";
+import Linkedin from "./icons/linkedin";
 
-type Icons = "plus" | "minus";
+type Icons =
+  | "plus"
+  | "minus"
+  | "whysignozcard"
+  | "shield"
+  | "datastorage"
+  | "nocompliancedocument"
+  | "shieldWithTick"
+  | "compliancedocumenttick"
+  | "share"
+  | "linkedin";
 
 const getIcons = (
   icons: Icons,
@@ -14,6 +32,22 @@ const getIcons = (
     case "minus": {
       return <Minus className={className} />;
     }
+    case "whysignozcard":
+      return <WhySignozCard className={className} />;
+    case "shield":
+      return <ShieldIcon className={className} />;
+    case "datastorage":
+      return <DataStorage className={className} />;
+    case "nocompliancedocument":
+      return <NoComplianceDocument className={className} />;
+    case "shieldWithTick":
+      return <ShieldWithTick className={className} />;
+    case "compliancedocumenttick":
+      return <ComplianceDocumentTick className={className} />;
+    case "share":
+      return <Share className={className} />;
+    case "linkedin":
+      return <Linkedin className={className} />;
     default: {
       throw new Error("Icon not found");
     }
