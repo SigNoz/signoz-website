fb5c7718b154f938fb764b9061cfc423e9e55438
Palash <palashgdev@gmail.com>
2022-07-26 17:23:13 +0530

    chore: jest configuration is added,next-config,tsconfig tailwind-config is updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,12 +17,40 @@ module.exports = {
       },
       colors: {
         "signoz-primary": "#F25733",
-        "dark-intermediate": "#404C5E",
+        "signoz-primary-light": "#F79A84",
+        "signoz-secondary-light": "#A3B4CB",
+        "signoz-dark-intermediate": "#404C5E",
+        "signoz-medium": "#132742",
+        "signoz-dark-light": "#74797F",
+        "signoz-pure": "#051326",
+        "signoz-light": "#FAFAFA",
+        "white-light": "#D9D9D9",
       },
+      backgroundImage: {
+        "hero-pattern-gradient":
+          "linear-gradient(0deg, rgba(19, 39, 66, 0.008) -18.5%, rgba(246, 170, 152, 0.128) 25.41%, rgba(250, 250, 250, 0.4) 104.41%),url('/images/whysignozmore.jpg')",
+      },
+      boxShadow: {
+        1: "0px 8px 24px 4px rgba(0, 0, 0, 0.05)",
+      },
+      borderWidth: {
+        DEFAULT: "1px",
+        0: "0",
+        2: "2px",
+        3: "3px",
+        4: "4px",
+        6: "6px",
+        8: "8px",
+      },
+    },
+    backgroundPosition: {
+      "bottom-1": "1rem",
+      "left-1": "1rem",
+      top: "1rem",
     },
   },
   experimental: {
     optimizeUniversalDefaults: true,
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
