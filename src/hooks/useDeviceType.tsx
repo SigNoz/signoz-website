0504adcfe93308dba000f80463ab0ea15ea3a481
Palash <palashgdev@gmail.com>
2022-07-26 17:31:19 +0530

    chore: styles for fonts and device type hook is added

---
+++ src/hooks/useDeviceType.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useMediaQuery } from "@react-hook/media-query";
+import * as React from "react";
+import { useContext, createContext } from "react";
+
+const MediaQueryContext = createContext("");
+
+export type DeviceType = "mobile" | "desktop";
+
+interface DeviceProviderProps {
+  type: DeviceType;
+  children: React.ReactElement;
+}
+
+const DeviceTypeProvider =
+  typeof window === "undefined"
+    ? ({ type, children }: DeviceProviderProps) => (
+        <MediaQueryContext.Provider value={type}>
+          {children}
+        </MediaQueryContext.Provider>
+      )
+    : ({ children }: DeviceProviderProps) => children;
+
+// For use only in server
+const useIsDesktopSSR = () => {
+  return useContext(MediaQueryContext) !== "mobile";
+};
+
+const useIsDesktop = () => {
+  if (typeof window === "undefined") {
+    return useIsDesktopSSR();
+  }
+  return useMediaQuery("only screen and (min-width: 768px)");
+};
+
+export { DeviceTypeProvider, useIsDesktop };
