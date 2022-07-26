620aa7498117797e75b70a6943a003401ce8a888
Palash <palashgdev@gmail.com>
2022-07-26 17:27:12 +0530

    chore: accordion component is added

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -3,7 +3,7 @@ import cx from "classnames";
 
 import getIcons from "asset/icons";
 
-const Accordion = ({ expandableText, text }: AccordionProps) => {
+const Accordion = ({ expandableText, text, className }: AccordionProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const onToggleHandler = useCallback(() => {
     setIsOpen((state) => !state);
@@ -15,9 +15,13 @@ const Accordion = ({ expandableText, text }: AccordionProps) => {
         boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.03)",
       }}
       onClick={onToggleHandler}
-      className={cx("flex py-6 px-4 bg-white cursor-pointer flex-col", {
-        "text-signoz-primary": isOpen,
-      })}
+      className={cx(
+        "flex py-6 px-4 bg-white cursor-pointer flex-col",
+        {
+          "text-signoz-primary": isOpen,
+        },
+        className
+      )}
     >
       <div className="flex">
         <div className="flex-1 font-openSans font-normal text-base leading-relaxed mr-3 ">
@@ -29,7 +33,7 @@ const Accordion = ({ expandableText, text }: AccordionProps) => {
       </div>
 
       {isOpen && (
-        <div className="text-dark-intermediate font-openSans text-sm mt-3">
+        <div className="text-signoz-dark-intermediate font-openSans text-sm mt-3">
           {expandableText}
         </div>
       )}
@@ -40,6 +44,7 @@ const Accordion = ({ expandableText, text }: AccordionProps) => {
 interface AccordionProps {
   text: string;
   expandableText: string;
+  className?: string;
 }
 
 export default Accordion;
