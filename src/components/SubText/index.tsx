7571fe984253f1e47e7ac18dcd95426185d2f8da
Palash <palashgdev@gmail.com>
2022-07-26 17:32:19 +0530

    feat: subtext atom is added

---
+++ src/components/SubText/index.tsx
@@ -0,0 +1,23 @@
+import cx from "classnames";
+
+const Subtext = ({ children, className, style }: SubtextProps): JSX.Element => {
+  return (
+    <div
+      className={cx(
+        "text-sm text-signoz-dark-intermediate leading-5 md:text-2xl",
+        className
+      )}
+      style={style}
+    >
+      <span>{children}</span>
+    </div>
+  );
+};
+
+interface SubtextProps {
+  children: React.ReactElement | string;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+export default Subtext;
