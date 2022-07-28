67c315b12ae5706f4d879aeb5c1fa47d494ec000
Palash <palashgdev@gmail.com>
2022-07-29 01:28:02 +0530

    react query is configured and state is hydrated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,21 +6,34 @@ import { ThemeProvider } from "next-themes";
 import { DeviceType, DeviceTypeProvider } from "hooks/useDeviceType";
 import { isMobile } from "lib/isMobileFromUA";
 import AppLayout from "container/AppLayout";
+import { useState } from "react";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { config } from "lib/react-query-config";
 
 interface MyAppProps extends AppProps {
   device: DeviceType;
 }
 function MyApp({ Component, pageProps, device }: MyAppProps) {
+  const [queryClient] = useState(() => new QueryClient(config));
+
   return (
-    <DeviceTypeProvider type={device}>
-      <ThemeProvider disableTransitionOnChange defaultTheme="light">
-        <AppLayout>
-          <MDXProvider components={components}>
-            <Component {...pageProps} />
-          </MDXProvider>
-        </AppLayout>
-      </ThemeProvider>
-    </DeviceTypeProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <DeviceTypeProvider type={device}>
+          <ThemeProvider disableTransitionOnChange defaultTheme="light">
+            <AppLayout>
+              <MDXProvider components={components}>
+                <Component {...pageProps} />
+              </MDXProvider>
+            </AppLayout>
+          </ThemeProvider>
+        </DeviceTypeProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
