414f7dbb5b58bf440bb7592923f63ebb998b2443
Palash <palashgdev@gmail.com>
2022-07-29 01:31:44 +0530

    chore: blog strip is updated

--- src/components/BlogStrip/index.tsx
+++ src/components/BlogStrip/index.tsx
@@ -4,12 +4,14 @@ const BlogStrip = (): JSX.Element => {
   return (
     <div className="bg-signoz-medium">
       <div className="py-4 px-6 text-center">
-        <span className="font-bold text-xs text-white">New blog post!</span>
+        <span className="font-bold text-xs text-white">
+          New blog post!&nbsp;
+        </span>
         <span className="font-semibold text-xs text-white">
           “Understand the performance of your applications and identify any
           potential problems before your users”
         </span>
-        <span className="font-semibold cursor-pointer text-signoz-primary-light">
+        <span className="font-semibold cursor-pointer text-xs text-signoz-primary-light">
           &nbsp;Learn more
         </span>
       </div>
