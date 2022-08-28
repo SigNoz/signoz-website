8ce7897cbadd0926a909acb10c71c79d3b09586d
Palash <palashgdev@gmail.com>
2022-08-28 21:40:02 +0530

    build is fixed

--- src/lib/generateRss.ts
+++ src/lib/generateRss.ts
@@ -1,13 +1,17 @@
+import { FrontMatterProps } from "./mdx";
 import metaData from "./metaData";
 import { escape } from "./utils/htmlParser";
 
-const generateRssItem = (post: any) => `
+const generateRssItem = (post: FrontMatterProps) => `
   <item>
     <guid>${metaData.siteUrl}/blog/${post.slug}</guid>
     <title>${escape(post.title)}</title>
     <link>${metaData.siteUrl}/blog/${post.slug}</link>
-    ${post.summary && `<description>${escape(post.summary)}</description>`}
-    <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+    ${
+      post.description &&
+      `<description>${escape(post.description)}</description>`
+    }
+    <pubDate>${new Date(post.date || "").toUTCString()}</pubDate>
     <author>${metaData.email} (${metaData.author})</author>
     ${
       post.tags &&
@@ -16,7 +20,7 @@ const generateRssItem = (post: any) => `
   </item>
 `;
 
-export const generateRss = (posts: any, page = "feed.xml") => `
+export const generateRss = (posts: FrontMatterProps[], page = "feed.xml") => `
   <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
       <title>${escape(metaData.title)}</title>
@@ -25,7 +29,9 @@ export const generateRss = (posts: any, page = "feed.xml") => `
       <language>${metaData.language}</language>
       <managingEditor>${metaData.email} (${metaData.author})</managingEditor>
       <webMaster>${metaData.email} (${metaData.author})</webMaster>
-      <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
+      <lastBuildDate>${new Date(
+        (posts[0] || {}).date || ""
+      ).toUTCString()}</lastBuildDate>
       <atom:link href="${
         metaData.siteUrl
       }/${page}" rel="self" type="application/rss+xml"/>
