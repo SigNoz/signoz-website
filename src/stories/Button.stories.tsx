e750e5b474e7972db7f6858bbfade1218da90527
Palash Gupta <palashgdev@gmail.com>
2022-11-01 02:03:35 +0530

    build is fixed

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -3,38 +3,16 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import Header from "components/Header";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "components/Header",
   component: Header,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } as ComponentMeta<typeof Header>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Header> = (args: any) => (
   <Header {...args} />
 );
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: "Button",
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: "Button",
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: "large",
-  label: "Button",
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: "small",
-  label: "Button",
+  isBlogStripOpen: false,
 };
