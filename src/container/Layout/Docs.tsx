c38a33751160350902a43bfd3b2afaec52c98967
Palash Gupta <palashgdev@gmail.com>
2022-11-01 00:59:28 +0530

    title is updated

--- src/container/Layout/Docs.tsx
+++ src/container/Layout/Docs.tsx
@@ -1,14 +1,30 @@
 import TOCInline from "components/MDX/components/TOCInline";
-import DocsSideBar from "container/DocsSidebar";
-import React, { useEffect, useState } from "react";
+import DocsSideBar, { DocsLinks } from "container/DocsSidebar";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import React, { useEffect, useMemo, useState } from "react";
 import { LayoutProps } from ".";
 
 const DocsLayout = ({ children, toc, docsLinks }: LayoutProps): JSX.Element => {
   const [isMounted, setIsMounted] = useState<boolean>(false);
 
+  const { asPath } = useRouter()
+
+
+  const allLinks = useMemo(() => {
+    const initialValue: DocsLinks[] = []
+    return docsLinks.reduce((acc, curr) => [...acc, curr, ...(curr?.subLinks?.map((subLink) => subLink) || [])], initialValue)
+  }, [docsLinks])
+
   useEffect(() => {
     setIsMounted(true);
   }, []);
+
+  const lastString = asPath.split("/").filter(e => Boolean(e));
+
+  const activeLink = allLinks.find(link => link.link === `/${lastString[lastString.length - 1]}`)
+
+  const title = `${activeLink?.name} | Signoz`
 
   return (
     <div className="flex flex-col md:flex-row">
@@ -18,6 +34,10 @@ const DocsLayout = ({ children, toc, docsLinks }: LayoutProps): JSX.Element => {
           <div className="min-h-screen hidden md:block inset-0 absolute border border-r-signoz-border z-[-1]" />
         </div>
       </div>
+
+      <Head>
+        <title>{title}</title>
+      </Head>
 
       {isMounted && (
         <div className="flex-[3] max-w-[60%] font-WorkSans px-4">
