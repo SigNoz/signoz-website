70a2e13be9cf744c44c4a8664a265db29bee2e6e
Palash <palashgdev@gmail.com>
2022-10-31 22:00:33 +0530

    blog strip component is updated

--- src/components/BlogStrip/index.tsx
+++ src/components/BlogStrip/index.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import CloseIcon from "./close.png";
+import NextImage from "next/image";
 
 const BlogStrip = (): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const onToggleHandler = (value: boolean) => {
+    return () => setIsOpen(value);
+  };
+
+  if (!isOpen) {
+    return <></>;
+  }
+
   return (
-    <div className="bg-signoz-medium">
-      <div className="py-4 px-6 text-center">
+    <div className="bg-signoz-medium flex justify-center">
+      <div className="py-4 px-6 text-center flex gap-3 w-full justify-center">
         <span className="font-bold text-xs text-white">
           New blog post!&nbsp;
         </span>
@@ -14,6 +26,12 @@ const BlogStrip = (): JSX.Element => {
         <span className="font-semibold cursor-pointer text-xs text-signoz-primary-light">
           &nbsp;Learn more
         </span>
+      </div>
+      <div
+        onClick={onToggleHandler(false)}
+        className="flex justify-center items-center w-16 cursor-pointer"
+      >
+        <NextImage width={16} height={16} src={CloseIcon} />
       </div>
     </div>
   );
