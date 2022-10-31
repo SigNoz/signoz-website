a2eb0ad3dd6533a77143da3bc19da7e5fd4a0fcd
Palash Gupta <palashgdev@gmail.com>
2022-11-01 01:23:54 +0530

    paragraph is updated

--- src/components/BlogTag/index.tsx
+++ src/components/BlogTag/index.tsx
@@ -1,33 +1,30 @@
 import { AllTags } from "lib/frontmatterToBlogData";
 import { displayTag } from "lib/tag";
-import { useRouter } from "next/router";
-import { useCallback } from "react";
+import Link from "next/link";
 
 interface BlogTagProps {
   tags: string[];
 }
 
 const BlogTag = ({ tags }: BlogTagProps): JSX.Element => {
-  const { push } = useRouter();
-
-  const onTagClickHandler = useCallback(
-    (tag: string) => () => {
-      push("/tags/[tag]", `/tags/${tag}`, { shallow: true, scroll: false });
-    },
-    [push]
-  );
-
   return (
     <div className="flex gap-2 md:gap-4 md:mx-0 mx-4 mt-4 flex-wrap overflow-x-auto">
       {tags.map((tag) => {
+        const values = displayTag[tag as AllTags]
         return (
-          <div
-            className="py-2 cursor-pointer rounded-[50px] px-3 font-openSans font-normal text-base text-white bg-signoz-primary"
-            key={tag}
-            onClick={onTagClickHandler(tag)}
+          <Link
+            key={values}
+            href="/tags/[tag]"
+            as={`/tags/${tag}`}
+            prefetch
+
+            passHref
+            aria-label={values}
           >
-            {displayTag[tag as AllTags]}
-          </div>
+            <a className="py-2 cursor-pointer rounded-[50px] px-3 font-openSans font-normal text-base text-white bg-signoz-primary text-ellipsis overflow-hidden whitespace-nowrap max-w-[200px]">
+              {values}
+            </a>
+          </Link>
         );
       })}
     </div>
--- src/components/ShareIcons/index.tsx
+++ src/components/ShareIcons/index.tsx
@@ -3,7 +3,7 @@ import getIcons from "asset/icons";
 const ShareIcons = ({ shareIcons }: SharedIconProps): JSX.Element => (
   <div className="p-4 md:p-0">
     <div className="text-base text-signoz-dark-light">Share this article</div>
-    <div className="flex items-center gap-4 mt-2">
+    <div className="flex items-center gap-4 mt-2 flex-wrap">
       {shareIcons.map((shareIcon) => {
         const { type, url } = shareIcon;
         return (
