a91a6de9f3bd896d00abe1c79d775b814b71bd82
Palash Gupta <palashgdev@gmail.com>
2022-11-07 22:54:20 +0530

    script is loaded in the worker

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import NextJsScript from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
       <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css"
+        <NextJsScript
+          strategy="worker"
+          src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css"
         />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
