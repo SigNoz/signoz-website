bc0fea121f7f4a02a2efb7d6d4d57764fe2d7898
Palash <palashgdev@gmail.com>
2022-11-08 11:57:02 +0530

    faq is updated

---
+++ src/container/FAQ/config.ts
@@ -0,0 +1,18 @@
+export const Faqs = [
+  {
+    question:
+      "I am looking for an application monitoring tool, is SigNoz an APM?",
+    answer:
+      "SigNoz is more than an APM. We provide all features like metrics and request traces which APMs provide. On top of that. we provide advanced filtering on trace data and custom aggregation on it",
+  },
+  {
+    question: "How does SigNoz compare to Jaeger?",
+    answer:
+      "Few ways in which SigNoz is more advanced than Jaeger : Jaeger UI doesn’t show any metrics on traces or on filtered traces, and Jaeger can’t get aggregates on filtered traces. For example, Cassandra doesn’t support Group By, Max, etc.",
+  },
+  {
+    question: "What will be your paid plan like?",
+    answer:
+      "SigNoz will be always open-source and free to be self-hosted for smaller teams. We will have role based Pricing for our enterprise edition which will have advanced features needed by bigger teams. Though for users who want hosted version of SigNoz, we do have cloud plans.",
+  },
+];
