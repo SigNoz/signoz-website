be2212c0437a0db811126c832d7b354670ba9473
Priyanka Chakraborty <chakraborty.priyanka@gmail.com>
2022-11-17 15:51:59 +0530

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -13,27 +13,27 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="w-[95%] pb-12 flex flex-row justify-evenly relative">
-        <div className="flex md:mt-8 md:flex-row flex-col md:gap-x-20 flex-wrap">
-          <div className="md:w-93px md:h-11 flex flex-col">
-            <div className="flex sm:flex-col sm:flex-wrap items-start mb-6 mt-4 md:px-2">
-              <NextImage
-                src={"/img/yc-logo.png"}
-                alt={"Y Combinator"}
-                width={80}
-                height={80}
-              />
-            </div>
-            <div className="md:w-3/5 mb-4  font-openSans font-semibold text-white text-lg">
-              Backed by Y Combinator
-            </div>
+      {/* <div className="w-[95%] pb-12 flex flex-row justify-evenly relative border"> */}
+      <div className="flex pb-14 px-8 md:mt-8 md:flex-row flex-col md:gap-20  justify-evenly flex-wrap relative">
+        <div className="md:w-93px md:h-11 flex flex-col">
+          <div className="flex sm:flex-col sm:flex-wrap items-start mb-6 mt-4 md:px-2">
+            <NextImage
+              src={"/img/yc-logo.png"}
+              alt={"Y Combinator"}
+              width={80}
+              height={80}
+            />
           </div>
-          <Link text="DOCS" subLinks={getSubLinks("DOCS")} />
-          <Link text="COMMUNITY" subLinks={getSubLinks("COMMUNITY")} />
-          <Link text="BLOGS" subLinks={getSubLinks("BLOGS")} />
-          <Link text="MORE" subLinks={getSubLinks("MORE")} />
+          <div className="md:w-3/5 mb-4  font-openSans font-semibold text-white text-lg">
+            Backed by Y Combinator
+          </div>
         </div>
+        <Link text="DOCS" subLinks={getSubLinks("DOCS")} />
+        <Link text="COMMUNITY" subLinks={getSubLinks("COMMUNITY")} />
+        <Link text="BLOGS" subLinks={getSubLinks("BLOGS")} />
+        <Link text="MORE" subLinks={getSubLinks("MORE")} />
       </div>
+      {/* </div> */}
     </footer>
   );
 };
