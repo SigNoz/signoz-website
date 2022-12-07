bc93c200ca49179e6603f85a1c9dd53eca2b55de
Priyanka Chakraborty <chakraborty.priyanka@gmail.com>
2022-12-07 13:40:14 +0530

    chore: applyLinkColor

--- src/container/PricingFAQ/config.ts
+++ src/container/PricingFAQ/config.ts
@@ -2,12 +2,12 @@ export const Faqs = [
   {
     question: "Do you offer enterprise support plans?",
     answer:
-      "Yes, feel free to reach out to us on <a mailto='hello@signoz.io'>hello@signoz.io</a>, if you need a dedicated support plan or paid support for setting your initial SigNoz setup",
+      "Yes, feel free to reach out to us on <a mailto='hello@signoz.io' style='color:Tomato;'>hello@signoz.io</a>, if you need a dedicated support plan or paid support for setting your initial SigNoz setup",
   },
   {
     question: "What type of features will be there in paid plans?",
     answer:
-      "The framework behind deciding which features will be in open source and which will be in enterprise plan is that if a feature is needed by an individual developer or small team running the product it should be in open source.<br/><br/>If a capability is uniquely valuable to enterprise requirements and are especially important when used in production and at scale like SSO, LDAP/SAML support,  audit logs, etc. would likely be in an enterprise plan.Read more on our thoughts on pricing <a href='https://signoz.notion.site/Our-Thoughts-on-Pricing-73f5e6939c1f42be905fe937b4107dad'>here</a>",
+      "The framework behind deciding which features will be in open source and which will be in enterprise plan is that if a feature is needed by an individual developer or small team running the product it should be in open source.<br/><br/>If a capability is uniquely valuable to enterprise requirements and are especially important when used in production and at scale like SSO, LDAP/SAML support,  audit logs, etc. would likely be in an enterprise plan. Read more on our thoughts on pricing <a href='https://signoz.notion.site/Our-Thoughts-on-Pricing-73f5e6939c1f42be905fe937b4107dad' target='_blank' style='color:Tomato;'><u><i>here</i></u></a>",
   },
   {
     question: "Who should use Enterprise plans?",
