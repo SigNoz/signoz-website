1a64571e1f3017627b021716bfdd6c350f8dcb78
Priyanka Chakraborty <chakraborty.priyanka@gmail.com>
2022-12-09 20:03:11 +0530

    Feat: handle-unorderedList

--- src/components/MDX/components/LI.tsx
+++ src/components/MDX/components/LI.tsx
@@ -1,6 +1,11 @@
-const LI = ({ children }: Props) => {
+import React from "react";
+
+const LI = ({ children, ...rest }: Props) => {
   return (
-    <li className="font-openSans my-2 list-decimal font-normal text-base leading-[160%] text-signoz-dark-intermediate">
+    <li
+      className="font-openSans my-2 font-normal text-base leading-[160%] text-signoz-dark-intermediate"
+      {...rest}
+    >
       {children}
     </li>
   );
